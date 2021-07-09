9291f0eaeff477bdb529d944804658659cb5cf77
root <root@a0chdan00.example.com>
2021-07-09 08:11:20 +0300

    test

--- test.py
+++ test.py
@@ -1,5 +1,12 @@
 import os
 from dotenv import load_dotenv
+import sqlite3
+
+import exceptions
+
+
+conn = sqlite3.connect(os.path.join("db", "reminders.db"))
+cursor = conn.cursor()
 
 load_dotenv()
 
@@ -7,3 +14,11 @@ API_TOKEN = os.getenv("API_TOKEN")
 ACCESS_ID = os.getenv("ACCESS_ID")
 
 print(API_TOKEN, ACCESS_ID)
+
+def test(table: str):
+    cursor.execute(f"SELECT * FROM {table}")
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row[0], row[4])
+
+test("reminder")
